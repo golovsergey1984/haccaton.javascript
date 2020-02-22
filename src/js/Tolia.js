fc8d12ec6221be7ebee8897c5d5b599c18cd85e2
Anatoliy <Blizzabum@Gmail.com>
2020-02-22 16:54:45 +0200

    commit

---
+++ src/js/Tolia.js
@@ -0,0 +1,23 @@
+import fetchSectionWeather from '../templates/section-weather.hbs';
+
+('use strict');
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+const key = 'bf7ee983cc3ce675dc38a2920f2d4f17';
+// weather?q=киев&lang=ru&units=metric
+
+export default {
+  fetchTopDiv(query) {
+    const requestParams = `${query}`;
+
+    //     return fetch(baseUrl + requestParams)
+    //         .then(response => response.json())
+    // }
+
+    return fetch(
+      `${baseUrl}forecast?q=${query}&appid=${key}&lang=ru&units=metric`,
+    )
+      .then(response => response.json())
+      .then(parsed => parsed.main());
+  },
+};
