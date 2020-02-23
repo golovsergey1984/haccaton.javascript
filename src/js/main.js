c01f8fa13dc845a39a0da20ededfc90bbe9ff1b6
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-02-23 15:06:49 +0200

    Sashas

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,21 @@
 'use strict';
 import topDivTemplate from '../templates/section-weather.hbs';
+import bottomDivTemplate from '../templates/sectionToday.hbs';
+
 import items from './services/fetchWeather.js';
 
 import fetchWeather from './services/fetchWeather.js';
 import fetchCities from './services/fetchCities.js';
 import fetchWeatherFiveDays from './services/fetchWeatherFiveDays.js';
-import wetherToday from './wetcherToday.js'
+import wetherToday from './wetcherToday.js';
 import { debounce } from 'lodash';
 
 const inputDiv = document.querySelector('.js-search');
 
 const list = document.querySelector('.list');
 const postItem = document.querySelector('.box_weather');
+const postItemBottom = document.querySelector('.box_today');
+
 inputDiv.addEventListener('input', _.debounce(createListWeatherHandler, 1000));
 
 function createListWeatherHandler(e) {
@@ -21,7 +25,8 @@ function createListWeatherHandler(e) {
     .then(data => {
       clearForm();
       buildtopDiv(data);
-      wetherToday.CreateTodayNode(data.sys.sunset,data.sys.sunrise);
+      buildBottomDiv(data);
+      /*  wetherToday.CreateTodayNode(data.sys.sunset, data.sys.sunrise); */
     })
     .catch(error => console.log(error));
 
@@ -39,6 +44,13 @@ function buildtopDiv(data) {
   const markup = topDivTemplate(data);
   postItem.insertAdjacentHTML('beforeend', markup);
 }
+
+function buildBottomDiv(data) {
+  const markup = bottomDivTemplate(data);
+  postItemBottom.insertAdjacentHTML('beforeend', markup);
+}
+
 function clearForm() {
   postItem.innerHTML = '';
+  postItemBottom.innerHTML = '';
 }
--- src/js/wetcherToday.js
+++ src/js/wetcherToday.js
@@ -1,69 +1,59 @@
-
 import sectionToday from '../templates/sectionToday.hbs';
 
 const sectionParrenNode = document.querySelector('.section_today');
 
-export default { CreateTodayNode (sunRise,sunSet){ 
-    if (sectionParrenNode !== '') {sectionParrenNode.innerHTML = '';}  
-    console.log('SunRISE:',sunRise);
-    const sunSetTime = new Date(sunSet).getUTCHours() +':'+ new Date(sunSet).getMinutes();    
-    console.log('Hours of sunset time:',sunSetTime);
-    const sunRiseTime = new Date(sunRise).getUTCHours() +':'+ new Date(sunRise).getMinutes();    
-    console.log('Hours of sunRise time:',sunRiseTime);
+export default {
+  CreateTodayNode(sunRise, sunSet) {
+    /*  if (sectionParrenNode !== '') {
+      sectionParrenNode.innerHTML = '';
+    } */
+
+    console.log('SunRISE:', sunRise);
+    const sunSetTime =
+      new Date(sunSet).getUTCHours() + ':' + new Date(sunSet).getMinutes();
+    console.log('Hours of sunset time:', sunSetTime);
+    const sunRiseTime =
+      new Date(sunRise).getUTCHours() + ':' + new Date(sunRise).getMinutes();
+    console.log('Hours of sunRise time:', sunRiseTime);
     const date = new Date();
-    const hours =  2 + date.getUTCHours();
+    const hours = 2 + date.getUTCHours();
     const min = date.getMinutes();
-    const currentTime = hours +":"+ min ;      
+    const currentTime = hours + ':' + min;
     const mounthDay = date.getDate();
     const mounth = date.getMonth();
-    const day = date.getDay();      
-    const dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const monthsArr =["January","February","March","April","May","June","July",
-    "August","September","October","November","December"]    
+    const day = date.getDay();
+    const dayArr = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    const monthsArr = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
     const dataToTemp = {
-        currentTime,        
-        mounthDay,
-        day: dayArr[day],
-        mounth: monthsArr[mounth],
-        sunset:sunSetTime,
-        sunrise:sunRiseTime,
+      currentTime,
+      mounthDay,
+      day: dayArr[day],
+      mounth: monthsArr[mounth],
+      sunset: sunSetTime,
+      sunrise: sunRiseTime,
     };
-    
+
     sectionParrenNode.insertAdjacentHTML('beforeend', sectionToday(dataToTemp));
-}} 
-
-// CreateTodayNode(1582366837,15823555140);
-
-// const searchInput = document.querySelector('input');
-
-// class   Search {
-//     constructor(parrentNode,){
-//         this.parrentNode = parrentNode;
-//         this.input = ''
-//         this.cityArr = []
-//     }
-//     dataTransfer(){
-//         return console.log('DATA TRANSER');
-//      }    
-   
-//    createHistory(city){       
-//         this.cityArr.push(city);       
-//         localStorage.setItem('history',JSON.stringify(this.cityArr))    ;
-
-//    }
-//     readHistory (){
-//         return JSON.parse (localStorage.getItem('history'));
-//     }
-//     createHistoryList (){
-        
-//     }
-
- 
-// }
-
-// 
-
-// const search = new Search (searchInput);
-// implament in async logic.
-
-// fetchWeather.fetchCountries();
+  },
+};
