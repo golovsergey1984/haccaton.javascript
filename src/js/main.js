970cf96907baae24ad992b943fd9d70f9e024be1
Mykhail1387 <melnychukmykhailo87@gmail.com>
2020-02-23 13:30:06 +0200

    last day

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,21 @@
 import fetchWeather from './services/fetchWeather.js';
 import fetchCities from './services/fetchCities.js';
 import fetchWeatherFiveDays from './services/fetchWeatherFiveDays.js';
+import { debounce } from 'lodash';
+
 
 const inputDiv = document.querySelector('.js-search');
 const list = document.querySelector('.list');
-inputDiv.addEventListener('input', createListWeatherHandler);
+inputDiv.addEventListener('input', _.debounce(createListWeatherHandler, 1000));
 
 function createListWeatherHandler(e) {
 
     const searchQuery = e.target.value;
-    // console.log(searchQuery)
     fetchWeather.fetchCountries(searchQuery)
         .then(data => {
             console.log('погода зараз:', data)
-            const sun = data.sys.sunrise;
-            console.log(sun)
-            const date = new Date(sun);
-            console.log(date);
 
-            const hours = date.getUTCHours(sun);
-            const min = date.getUTCMinutes(sun);
-            const sunrise = `${hours}:${min}`;
-            console.log(sunrise);
+
 
 
 
@@ -31,7 +25,7 @@ function createListWeatherHandler(e) {
 
     fetchCities.fetchImage(searchQuery)
         .then(data => {
-            const imageCity = data[2].largeImageURL
+            const imageCity = data[0].largeImageURL
             const body = document.querySelector('body');
             body.style.cssText = `background-image: url("${imageCity}"); background-size: cover;
             `
@@ -44,10 +38,20 @@ function createListWeatherHandler(e) {
 
 
 
-// const date = new Date(1582347461);
-// console.log(date);
+// const date = new Date(1582392390);
+// console.log('set', date);
 
 // const dateSunset = new Date(1582385140);
 // console.log(date);
 
 
+// console.log('погода зараз:', data)
+// const sun = data.sys.sunrise;
+// console.log(sun)
+// const date = new Date(sun);
+// console.log(date);
+
+// const hours = date.getUTCHours(sun);
+// const min = date.getUTCMinutes(sun);
+// const sunrise = `${hours}:${min}`;
+// console.log(sunrise);
--- src/js/services/fetchWeather.js
+++ src/js/services/fetchWeather.js
@@ -2,20 +2,19 @@
 
 const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const key = 'bf7ee983cc3ce675dc38a2920f2d4f17';
-// weather?q=киев&lang=ru&units=metric
 
 export default {
     fetchCountries(query) {
         const requestParams = `${query}`;
 
-        //     return fetch(baseUrl + requestParams)
-        //         .then(response => response.json())
-        // }
-
         return fetch(`${baseUrl}weather?q=${query}&appid=${key}&lang=ru&units=metric`)
             .then(response => response.json())
+            .then(parsed => {
+                console.log('code:', parsed.cod)
+                if (parsed.cod === "404") {
+                    alert('Веедите корректное название города, если хотите узнать погоду!')
+                }
+                return parsed.main
+            })
     }
 }
-
-
-
