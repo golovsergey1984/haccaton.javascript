5c49d68834d50c0e2e2f44b08894da6e4ee85843
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-02-25 21:11:06 +0200

    from master

--- src/js/services/fetchWeather.js
+++ src/js/services/fetchWeather.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+const baseUrl =
+  'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/';
 const key = 'bf7ee983cc3ce675dc38a2920f2d4f17';
 
 export let cityWeather = {
@@ -17,9 +18,3 @@ export function fetchWeatherByCity(query) {
       return weather;
     });
 }
-
-
-
-
-
-
