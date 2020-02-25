c89a717df408f2b668ebf9e87e61fae049f5bc05
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-02-25 22:06:27 +0200

    div centered

--- src/js/services/fetchWeather.js
+++ src/js/services/fetchWeather.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const baseUrl = 'api.openweathermap.org/data/2.5/';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const key = 'bf7ee983cc3ce675dc38a2920f2d4f17';
 
 export let cityWeather = {
