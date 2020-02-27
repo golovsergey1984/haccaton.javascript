3805d271b9b48f76421455d75ea86fc22433439f
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-02-27 19:57:01 +0200

    19:56 hi added

--- src/js/geoposition.js
+++ src/js/geoposition.js
@@ -6,41 +6,36 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/';
 const key = 'bf7ee983cc3ce675dc38a2920f2d4f17';
 
 function fetchWeatherByCityName(lat, lon) {
-    const url = `${baseUrl}weather?lat=${lat}&lon=${lon}&appid=${key}&lang=en&units=metric`;
+  const url = `${baseUrl}weather?lat=${lat}&lon=${lon}&appid=${key}&lang=en&units=metric`;
 
-    return fetch(url)
-        .then(response => response.json())
+  return fetch(url).then(response => response.json());
 }
 
 //===geolocation=====
 const getCurrentPosition = () => {
-
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-            resolve,
-            reject
-        );
-    })
-}
-getCurrentPosition().then(location => {
-    success(location)
-}).catch(error => console.log(error))
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+getCurrentPosition()
+  .then(location => {
+    success(location);
+  })
+  .catch(error => console.log(error));
 
 function success(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
 
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+  const latitudeToFixed = latitude.toFixed(2);
+  const longitudeToFixed = longitude.toFixed(2);
 
-    const latitudeToFixed = latitude.toFixed(2);
-    const longitudeToFixed = longitude.toFixed(2);
+  fetchWeatherByCityName(latitudeToFixed, longitudeToFixed).then(data => {
+    let cityName = data.name;
+    console.log('your city name:', cityName + 'hi');
 
-    fetchWeatherByCityName(latitudeToFixed, longitudeToFixed)
-        .then(data => {
-            let cityName = data.name;
-            console.log('your city name:', cityName)
+    fetchAndRenderCityByQuery(cityName);
 
-            fetchAndRenderCityByQuery(cityName);
-
-            fetchAndRenderCityImage(cityName);
-        })
+    fetchAndRenderCityImage(cityName);
+  });
 }
